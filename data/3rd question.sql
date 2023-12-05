c85feb2af42b505289128b0bde46515d22d59beb
Syed Wasim Ahmed <90907275+syedwasimahmed2803@users.noreply.github.com>
2023-12-05 12:19:47 +0530

    Update 3rd question.sql

--- data/3rd question.sql
+++ data/3rd question.sql
@@ -1,12 +1,7 @@
 USE IPL;
-SELECT * from matches;
 
-SELECT
-  season,
-  winner AS team,
-  COUNT(*) AS matches_won
-FROM
-  matches
-GROUP BY
-  season, winner;
-
+SELECT d.bowling_team,SUM(d.extra_runs) AS extra_runs_conceded
+FROM deliveries d
+JOIN matches m ON d.match_id = m.id
+WHERE m.season = '2016'
+GROUP BY d.bowling_team;
